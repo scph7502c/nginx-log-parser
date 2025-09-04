ff48472054274f5a8dfc2eb9a4bf15fe281aff9b
damian <damian.temimi@gmail.com>
2025-09-04 21:35:17 +0200

    made regex for extract dict from log file

--- nginx_log_parser.py
+++ nginx_log_parser.py
@@ -1,39 +1,84 @@
 import os
+import re
+from datetime import datetime
 from pathlib import Path
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-NGINX_LOG_FILE = os.environ.get("NGINX_LOG_FILE")
+# to do: create function to get names ov various nginx log files, fe. my-website.access.log, some-website.access.log, my-website.error.log etc.
+NGINX_ACCESS_LOG_FILE = os.environ.get("NGINX_ACCESS_LOG_FILE")
+NGINX_ERROR_LOG_FILE = os.environ.get("NGINX_ERROR_LOG_FILE")
+log_files = (NGINX_ACCESS_LOG_FILE, NGINX_ERROR_LOG_FILE)
 log_line = []
 log_list = []
+
+today_date = datetime.today().strftime("%Y-%m-%d")
+print(today_date)
+
+
+def print_nginx_log_dir_content(nginx_log_dir):
+    # curr_working_dir = os.getcwd()
+    try:
+        log_files_in_dir = os.listdir(nginx_log_dir)
+        print(log_files_in_dir)
+        for l in log_files_in_dir:
+            print(l)
+    except FileNotFoundError:
+        print("File not found!")
+    except PermissionError:
+        print("Permission denied!")
+    except NotADirectoryError:
+        print("Not a directory!")
 
 
 def read_whole_log_file(filename):
     contents = path.read_text()
     contents = contents.rstrip()
-    print(contents)
+    return contents
 
 
-def sanitize_line(line):
-    line = line.strip()
-    line = line.split(" ")
-    return line
-
-
-def read_line_by_line_log_file(filename):
+def create_log_list_of_lists(filename):
     with open(filename, "r", encoding="utf-8") as f:
         for line in f:
-            sanitized_line = sanitize_line(line)
-            log_line.append(sanitized_line)
-            print(f"IP: {line[0]}, status code: {line[8]}")
-            log_list.append(sanitized_line)
-            log_line.clear()
-    for line in log_list:
-        print(line)
+            log_list.append(create_log_dict_of_lists(line))
+    for l in log_list:
+        print(l)
+    return log_list
+
+
+def create_log_dict_of_lists(log_lists_line):
+    log_pattern = re.compile(
+        r"^(?P<ip>\S+) (?P<identd>\S+) (?P<user>\S+) (\[(?P<time_local>[^]]+)]) "
+    )
+    match = log_pattern.match(log_lists_line)
+    return match.groupdict()
+
+
+def main():
+    while True:
+        print("****NGINX LOG PARSER****")
+
+        log_dir = input("Enter log files path: ")
+        print("1. Analyze chosen log file")
+        print("2. Exit")
+
+        user_choice = input("Choose your option: ")
+
+        if user_choice == "1":
+            if log_dir != "":
+                print(log_dir)
+                print_nginx_log_dir_content(log_dir)
+            else:
+                print("You have to enter log file path!")
+        elif user_choice == "2":
+            print("Bye!")
+            exit(0)
 
 
 if __name__ == "__main__":
-    path = Path(NGINX_LOG_FILE)
-    read_line_by_line_log_file(path)
+    path = Path(NGINX_ACCESS_LOG_FILE)
+    create_log_list_of_lists(path)
+    # print_current_dir()
+    # main()
